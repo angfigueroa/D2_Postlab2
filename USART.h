d5dbbee020eb2d1baf111624300c60d140ea3d69
angfigueroa <125718358+angfigueroa@users.noreply.github.com>
2023-07-25 22:09:03 -0600

    Create USART.h

---
+++ USART.h
@@ -0,0 +1,7 @@
+#ifndef USART_H
+#define	USART_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+void USART_CONF(int frec, tr, rc);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
